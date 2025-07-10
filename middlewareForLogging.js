a49b9469582faf116b0833322e97fb71e362273d
manu <manu@gmail.com>
2025-07-10 13:56:30 +0530

    Create a Server with Middleware for Logging

---
+++ middlewareForLogging.js
@@ -0,0 +1,36 @@
+import express from 'express';
+
+const app = express();
+const PORT = 4000;
+
+
+app.use((req,res,next)=>{
+
+
+    console.log(`${req.method} request for '${req.url}'`);
+    next();
+})
+
+app.get('/products', (req, res) => {
+   res.send('Here is the list of all products.');
+});
+app.post('/products', (req, res) => {
+   res.send('A new product has been added.');
+});
+
+app.get('/categories', (req, res) => {
+   res.send('Here is the list of all categories.');
+});
+app.post('/categories', (req, res) => {
+   res.send('A new category has been created.');
+});
+
+// Handle unknown routes
+app.use((req, res) => {
+    res.status(404).send('<h1>404 Not Found</h1><p>The requested resource was not found on this server.</p>');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
